5fb48e49ae8435a3dd26dd4b3d1ab503051b0458
EvgenijZ <leetruk12@gmail.com>
2022-01-23 14:10:13 +0200

    Upload brand page

--- pages/brands.jsx
+++ pages/brands.jsx
@@ -1,6 +1,7 @@
 import page_style from '../styles/page.module.scss';
 import brands_style from '../styles/brand.module.scss';
 import Tab, { TabItem, TabHead, TabBody } from '../components/tab';
+import Image from 'next/image';
 
 export default function Brands() {
   return (
@@ -10,10 +11,193 @@ export default function Brands() {
       </div>
       <div className={page_style.page}>
         <div className={page_style.page__wrap}>
-          <Tab boxed centered>
+          <Tab centered>
             <TabItem active>
-              <TabHead>1 этап</TabHead>
-              <TabBody>1</TabBody>
+              <TabHead>Все</TabHead>
+              <TabBody>
+                <div className={brands_style.brands__list}>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand1.svg'
+                      alt='Brand 1'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand2.svg'
+                      alt='Brand 2'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand3.png'
+                      alt='Brand 3'
+                      width='160'
+                      height='60'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand4.svg'
+                      alt='Brand 4'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand5.png'
+                      alt='Brand 5'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand6.svg'
+                      alt='Brand 6'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand7.svg'
+                      alt='Brand 7'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand8.svg'
+                      alt='Brand 8'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand9.svg'
+                      alt='Brand 9'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand10.svg'
+                      alt='Brand 10'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                </div>
+              </TabBody>
+            </TabItem>
+            <TabItem>
+              <TabHead>Виски</TabHead>
+              <TabBody>
+                <div className={brands_style.brands__list}>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand1.svg'
+                      alt='Brand 1'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand3.png'
+                      alt='Brand 3'
+                      width='160'
+                      height='60'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand6.svg'
+                      alt='Brand 6'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand8.svg'
+                      alt='Brand 8'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand10.svg'
+                      alt='Brand 10'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                </div>
+              </TabBody>
+            </TabItem>
+            <TabItem>
+              <TabHead>Ром</TabHead>
+              <TabBody>
+                <div className={brands_style.brands__list}>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand2.svg'
+                      alt='Brand 2'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                </div>
+              </TabBody>
+            </TabItem>
+            <TabItem>
+              <TabHead>Текила</TabHead>
+              <TabBody>
+                <div className={brands_style.brands__list}>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand4.svg'
+                      alt='Brand 4'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                </div>
+              </TabBody>
+            </TabItem>
+            <TabItem>
+              <TabHead>Водка</TabHead>
+              <TabBody>
+                <div className={brands_style.brands__list}>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand5.png'
+                      alt='Brand 5'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                  <div className={brands_style.brands__item}>
+                    <Image
+                      src='/static/images/brands/brand7.svg'
+                      alt='Brand 7'
+                      width='160'
+                      height='160'
+                    />
+                  </div>
+                </div>
+              </TabBody>
             </TabItem>
           </Tab>
         </div>
